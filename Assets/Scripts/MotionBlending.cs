8116284cd8b0a7ab169189f09a1d5d571afbca44
AshwinMods <ashwin.mods@gmail.com>
2022-07-13 02:43:36 +0530

    Motion Trails

--- Assets/Scripts/MotionBlending.cs
+++ Assets/Scripts/MotionBlending.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MotionBlur : MonoBehaviour 
+public class MotionBlending : MonoBehaviour 
 {
 	[SerializeField] Transform m_Target;
-	[SerializeField] int m_MaxDrawCount = 5;
+	[SerializeField] int m_DrawCount = 5;
+	[SerializeField] float m_MotionDelta = 1f / 300f;
+	[SerializeField] AnimationCurve m_TrailAlphaCurve;
+	[SerializeField] Rigidbody m_TargetRB;
 
 	public struct MeshToDraw
 	{
@@ -23,6 +26,7 @@ public class MotionBlur : MonoBehaviour
 	List<List<Matrix4x4>> m_PhysicsFrameMtxList = new List<List<Matrix4x4>>();
 	private void Awake()
 	{
+		m_TargetRB = m_Target.GetComponent<Rigidbody>();
 		foreach (Transform l_Trans in m_Target)
 		{
 			var l_MeshFilter = l_Trans.GetComponent<MeshFilter>();
@@ -35,38 +39,27 @@ public class MotionBlur : MonoBehaviour
 			}
 		}
 	}
-	private void FixedUpdate()
-	{
-		int l_Index = 0;
-		foreach (var l_M2D in m_MeshToDraw)
-			m_PhysicsFrameMtxList[l_Index++].Add(l_M2D.m_Trans.localToWorldMatrix);
-	}
-	private void Update()
-	{
-		int l_Index = 0;
-		foreach (var l_M2D in m_MeshToDraw)
-			m_PhysicsFrameMtxList[l_Index++].Add(l_M2D.m_Trans.localToWorldMatrix);
-	}
 	private void OnPostRender()
 	{
-		int l_ObjIndex = 0;
 		foreach (var l_M2D in m_MeshToDraw)
 		{
-			var l_PhxMtx = m_PhysicsFrameMtxList[l_ObjIndex++];
-			if (l_PhxMtx.Count > m_MaxDrawCount)
-				//l_PhxMtx.RemoveRange(0, l_PhxMtx.Count - m_MaxDrawCount);
-				while (l_PhxMtx.Count > m_MaxDrawCount)
-					l_PhxMtx.RemoveAt(0);
-			float l_AlphaDT = 1f / l_PhxMtx.Count;
-			float l_Alpha = 0f;
-			//foreach (var l_Mtx in l_PhxMtx)
-			for (int i = 0; i < l_PhxMtx.Count; i++)
+			var l_OAng = m_TargetRB.angularVelocity * Mathf.Rad2Deg * m_MotionDelta * m_DrawCount;
+			var l_fRot = l_M2D.m_Trans.eulerAngles;
+			var l_iRot = l_fRot - l_OAng;
+
+			float l_Ratio = 0f;
+			float l_RatioDT = 1f / m_DrawCount;
+			for (int i = 0; i < m_DrawCount; i++)
 			{
-				var l_Mtx = l_PhxMtx[i];
-				l_M2D.m_Material.SetPass(0);
+				l_Ratio += l_RatioDT;
 				var c = l_M2D.m_Material.color;
-				c.a = Mathf.Lerp(0, 1, (l_Alpha += l_AlphaDT));
+				c.a = m_TrailAlphaCurve.Evaluate(l_Ratio);
 				l_M2D.m_Material.color = c;
+
+				var l_Rot = l_fRot;
+				l_Rot.y = Mathf.Lerp(l_iRot.y, l_fRot.y, l_Ratio);
+				var l_Mtx = Matrix4x4.TRS(l_M2D.m_Trans.position, Quaternion.Euler(l_Rot), l_M2D.m_Trans.lossyScale);
+				l_M2D.m_Material.SetPass(0);
 				Graphics.DrawMeshNow(l_M2D.m_Mesh, l_Mtx);
 			}
 		}
