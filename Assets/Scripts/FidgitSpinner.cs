4cc616e3c2ff1b0d67f3e92f054a7eef45d9be49
ashwaniappsc <ashwani@appsoleutcoders.com>
2022-07-13 11:36:25 +0530

    Mouse Interaction and Cam Rotation

--- Assets/Scripts/FidgitSpinner.cs
+++ Assets/Scripts/FidgitSpinner.cs
@@ -5,8 +5,13 @@ using UnityEngine.EventSystems;
 
 public class FidgitSpinner : MonoBehaviour
 {
+	[Header("Reference")]
+	[SerializeField] Camera m_Cam;
+	[SerializeField] Transform m_CamPivot;
+
 	[Header("Config")]
 	[SerializeField] float m_AngVel_Mul = 1;
+	[SerializeField] float m_CamMove_Senstivity = 0.1f;
 
 	[Header("Runtime Vars")]
 	[SerializeField] bool m_IsSpinned;
@@ -24,14 +29,28 @@ public class FidgitSpinner : MonoBehaviour
 	Transform m_Trans;
 	Rigidbody m_Rigid;
 
+	Vector3 m_TouchOffset;
+	Vector3 m_MouseDrag;
+
+	float m_LastAngleDelta;
+
 	#region Unity Callbacks
 	private void Awake()
 	{
 		m_Trans = transform;
 		m_Rigid = GetComponent<Rigidbody>();
+		m_Rigid.maxAngularVelocity = 6E6F;
 	}
 	private void Update()
 	{
+		if (Input.GetMouseButtonDown(1))
+			m_MouseDrag = Input.mousePosition;
+		if (Input.GetMouseButton(1))
+		{
+			var l_MDelta = Input.mousePosition - m_MouseDrag;
+			m_CamPivot.eulerAngles += (Vector3.up * l_MDelta.x * m_Cam.aspect + Vector3.left * l_MDelta.y) * m_CamMove_Senstivity;
+			m_MouseDrag = Input.mousePosition;
+		}
 	}
 	private void OnValidate()
 	{
@@ -49,7 +68,8 @@ public class FidgitSpinner : MonoBehaviour
 	void Spin()
 	{
 		m_IsSpinned = true;
-		m_Rigid.angularVelocity = m_Trans.up * m_PointerDelta.magnitude * m_AngVel_Mul;
+		//m_Rigid.angularVelocity = m_Trans.up * m_PointerDelta.magnitude * m_AngVel_Mul;
+		m_Rigid.angularVelocity = m_Trans.up * m_LastAngleDelta * m_AngVel_Mul;
 		m_PointerDelta = Vector2.zero;
 	}
 	#endregion
@@ -57,19 +77,31 @@ public class FidgitSpinner : MonoBehaviour
 	#region Events
 	public void OnMouseDown()
 	{
+		if (!Input.GetMouseButton(0) || Input.GetMouseButton(1)) return;
 		m_IsSpinned = false;
 		m_IsPointerDown = true;
 		m_IsPointerExit = false;
 		m_PointerDelta = Vector2.zero;
 		m_PointerPosition = Input.mousePosition;
+		Physics.Raycast(m_Cam.ScreenPointToRay(Input.mousePosition + Vector3.forward), out RaycastHit l_HitInfo);
+		m_TouchOffset = l_HitInfo.point - m_Trans.position;
+		m_Rigid.angularVelocity = Vector3.zero;
 	}
 	public void OnMouseDrag()
 	{
+		if (!Input.GetMouseButton(0) || Input.GetMouseButton(1)) return;
 		m_PointerDelta = (Input.mousePosition - m_PointerPosition);
 		m_PointerPosition = Input.mousePosition;
+
+		Physics.Raycast(m_Cam.ScreenPointToRay(Input.mousePosition + Vector3.forward), out RaycastHit l_HitInfo);
+		var l_DragOffset = l_HitInfo.point - m_Trans.position;
+		m_LastAngleDelta = Vector3.SignedAngle(m_TouchOffset, l_DragOffset, Vector3.up);
+		m_Trans.eulerAngles += Vector3.up * m_LastAngleDelta;
+		m_TouchOffset = l_DragOffset;
 	}
 	public void OnMouseUp()
 	{
+		if (Input.GetMouseButton(1)) return;
 		m_IsPointerDown = false;
 		if (!m_IsSpinned)
 			Spin();
